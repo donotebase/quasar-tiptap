047567068324084d3d695f14fc50726e221561d2
Micle Bu <mekery@qq.com>
2020-08-22 15:21:28 +0800

    style(example): Hide toolbay in readonly mode by default.

--- src/pages/examples/readonly.vue
+++ src/pages/examples/readonly.vue
@@ -24,6 +24,7 @@ export default {
       options: {
         content: BasicFeaturesArticle,
         editable: false,
+        showToolbar: false,
         extensions: [
           ...RecommendedExtensions
         ]
